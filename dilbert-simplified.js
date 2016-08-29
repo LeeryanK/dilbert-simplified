19d9d9e43fd25d640b53902e1ed72c6720f4c729
Kyle Lin <kylejlin.us@gmail.com>
2016-08-28 18:57:46 -0700

    Finished first draft of dilbert-simplified.js

--- dilbert-simplified.js
+++ dilbert-simplified.js
@@ -1,1 +1,169 @@
+(function() {
+  var btns = {
+    first: document.getElementById('first'),
+    previous: document.getElementById('previous'),
+    random: document.getElementById('random'),
+    next: document.getElementById('next'),
+    newest: document.getElementById('newest')
+  };
 
+  var comicImg = document.getElementById('comic');
+
+  var BASE = 'http://www.foo.be/docs-free/dilbert/www.geek.nl/pics/dilbert-arch/dilbert-';
+  var FIRST = DateString(20011103);
+  var NEWEST = DateString(20100815);
+
+  var currentComicDate;
+
+  function DateString(src) {
+    if (!(this instanceof DateString)) return new DateString(src);
+
+    src = ('' + src).split(/[^0-9]/).join('');
+
+    if (src.length !== 8)
+      throw new SyntaxError('Invalid source string passed to DateString constructor');
+
+    this.string = src;
+  }
+
+  DateString.prototype.getYear = function() {
+    return this.string.slice(0, 4);
+  };
+
+  DateString.prototype.getMonth = function() {
+    return this.string.slice(4, 6);
+  };
+
+  DateString.prototype.getDay = function() {
+    return this.string.slice(6, 8);
+  };
+
+  DateString.prototype.getNextDay = function() {
+    // Note: Years, months, and days are one-based for indices.
+    // February is assigned conditionally because its length in days varies (due
+    //   to leap years).
+    var DAYS_IN_EACH_MONTH = [31, +this.getYear() % 4 ? 28 : 29, 31, 30, 31, 30,
+      31, 31, 30, 31, 30, 31];
+    var MONTHS_IN_YEAR = 12;
+
+
+    var lastDayInThisMonth = DAYS_IN_EACH_MONTH[+this.getMonth() - 1];
+
+    var thisYear = +this.getYear();
+    var thisMonth = +this.getMonth();
+    var thisDay = +this.getDay();
+
+    var tmrwDay = thisDay + 1;
+    var tmrwMonth = thisMonth;
+    var tmrwYear = thisYear;
+
+    if (tmrwDay > lastDayInThisMonth) {
+      tmrwDay = 1;
+      tmrwMonth++;
+
+      if (tmwrMonth > MONTHS_IN_YEAR) {
+        tmrwMonth = 1;
+        tmrwYear++;
+      }
+    }
+
+    return new DateString(lp0(tmrwYear, 4) + lp0(tmrwMonth, 2) +
+      lp0(tmrwDay, 2));
+  };
+
+  DateString.prototype.getPreviousDay = function() {
+    // Note: Years, months, and days are one-based for indices.
+    // February is assigned conditionally because its length in days varies (due
+    //   to leap years).
+    var DAYS_IN_EACH_MONTH = [31, +this.getYear() % 4 ? 28 : 29, 31, 30, 31, 30,
+      31, 31, 30, 31, 30, 31];
+    var MONTHS_IN_EACH_YEAR = 12;
+
+
+    var lastDayInPreviousMonth = DAYS_IN_EACH_MONTH[+this.getMonth() - 2];
+
+    var thisYear = +this.getYear();
+    var thisMonth = +this.getMonth();
+    var thisDay = +this.getDay();
+
+    var yesterDay = thisDay - 1;
+    var yesterMonth = thisMonth;
+    var yesterYear = thisYear;
+
+    if (yesterDay < 1) {
+      yesterDay = lastDayInPreviousMonth;
+      yesterMonth--;
+
+      if (yesterMonth < 1) {
+        yesterMonth = MONTHS_IN_EACH_YEAR;
+        yesterYear--;
+      }
+    }
+
+    return new DateString(lp0(yesterYear, 4) + lp0(yesterMonth, 2) +
+      lp0(yesterDay, 2));
+  };
+
+  currentComicDate = FIRST;
+
+  function lp0(src, len) {
+    src += '';
+    return src.length < len ? '0'.repeat(len - src.length) + src : src;
+  }
+
+  function randInt(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
+
+  function loadComicAccordingToComicDate() {
+    comicImg.src = BASE + currentComicDate.string + '.gif';
+  }
+
+  function nextComic() {
+    if (!currentComicDate.equals(NEWEST)) {
+      currentComicDate = currentComicDate.getNextDay();
+      loadComicAccordingToComicDate();
+    }
+  }
+
+  function previousComic() {
+    if (!currentComicDate.equals(FIRST)) {
+      currentComicDate = currentComicDate.getPreviousDay();
+      loadComicAccordingToComicDate();
+    }
+  }
+
+  function firstComic() {
+    currentComicDate = FIRST;
+    loadComicAccordingToComicDate();
+  }
+
+  function newestComic() {
+    currentComicDate = NEWEST;
+    loadComicAccordingToComicDate();
+  }
+
+  function randomComic() {
+    var year = randInt(FIRST.getYear() + 1, NEWEST.getYear() - 1);
+    var month = randInt(1, 12);
+    var DAYS_IN_EACH_MONTH = [31, year % 4 ? 28 : 29, 31, 30, 31, 30,
+      31, 31, 30, 31, 30, 31];
+    var day = randInt(1, DAYS_IN_EACH_MONTH[month - 1]);
+
+    var str = '' + year + lp0(month, 2) + lp0(day, 2);
+
+    currentComicDate = DateString(str);
+    loadComicAccordingToComicDate();
+  }
+
+  function listen(elem, func) {
+    elem.addEventListener('click', func);
+    elem.addEventListener('touchend', func);
+  }
+
+  listen(btns.first, firstComic);
+  listen(btns.previous, previousComic);
+  listen(btns.random, randomComic);
+  listen(btns.next, nextComic);
+  listen(btns.newest, newestComic);
+})();
